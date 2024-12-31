5e1f1df715a019dccc41d48bb493d640568aa9d2
dankags <danielkagombe70@gmail.com>
2024-12-31 17:56:24 +0300

    integrated livekit video confrencing tool which enables the app to perform live video chatting.

--- src/app/(pages)/appointments/meetup/[appointmentId]/page.tsx
+++ src/app/(pages)/appointments/meetup/[appointmentId]/page.tsx
@@ -1,5 +1,7 @@
+import VideoLayout from '@/components/videoConfrencing/VideoLayout';
 import { Metadata } from 'next';
 import React from 'react'
+
 
 export const metadata: Metadata = {
   title: "Video confrence meeting",
@@ -7,8 +9,11 @@ export const metadata: Metadata = {
     "This is the page where the patient and the doctor meet up virtualy through video confrencing.But the patient must pay first in order to activate the video confrence",
 };
 
-export default function VideoMeetUp() {
+export default function VideoMeetUpPage() {
+
   return (
-    <div>VideoMeetUp</div>
+    <div className="w-full h-screen min-h-screen flex-col px-3  xl:px-12 2xl:px-32 ">
+      <VideoLayout/>
+    </div>
   )
 }
